9091a878d12ed72f16cc08609cbd7d8a49a85de2
Top Lap <lauris.savas203@gmail.com>
2025-05-09 21:55:28 +0300

    footer made

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -15,6 +15,29 @@ function App() {
       <main>
         <p>Welcome to Food Force! This is the main content area.</p>
       </main>
+      <footer> 
+        <div>
+          <div className="footer_icon">
+            <a href="https://www.facebook.com/"><div className='fb-icon icons'></div></a>
+            <a href="https://www.instagram.com/"><div className='insta-icon icons'></div></a>
+            <a href="https://www.x.com/"><div className='x-icon icons'></div></a>
+          </div>
+          <div className='footer_brands'>
+            <p>&#169; Copyright "FoodForce" 2025. All Rights Reserved </p>
+            <a href="#"><div className='companyLogo '></div></a>
+          </div>
+          <div className="footer_info"> 
+            <p>Contacts: +370555777123</p>
+            <p>Email: FoodForce@sportfood.eat</p>
+            <p>Adress: Kazkokia g.</p>
+            <p>Work hours: I-VII 8:00-18:00</p>
+          </div>
+        </div>
+        
+        
+         
+        
+      </footer>
     </div>
   );
 }
